5606e6c49c562f2d3b5a00f4df8a78d9fe0397cc
茂春 郭 <haru@midget.local>
2016-12-31 10:05:32 +0900

    first commit

---
+++ js/nardove.js
@@ -0,0 +1,38 @@
+var NARDOVE = NARDOVE || {};
+
+NARDOVE.Main = (function() {
+
+	paper.install(window);
+	paper.setup('canvas');
+
+	var timer = new Date();
+	var addJellyTimer = 0;
+	var jellyCounter = 0;
+	var numJellies = 7;
+	var jellies = [numJellies];
+	var jellyResolution = 14;
+
+
+	window.onload = function() {
+		view.onFrame = draw;
+	};
+
+
+	this.draw = function(event) {
+		if (event.time > addJellyTimer + 6 && jellyCounter < numJellies) {
+			jellySize = Math.random() * 10 + 40;
+			jellies[jellyCounter] = new NARDOVE.Jelly(jellyCounter, jellySize, jellyResolution);
+			jellies[jellyCounter].init();
+
+			jellyCounter++;
+			addJellyTimer = event.time;
+		}
+
+		if (jellyCounter > 0) {
+			for (var j = 0; j < jellyCounter; j++) {
+				jellies[j].update(event);
+			}
+		}
+	};
+
+})();
